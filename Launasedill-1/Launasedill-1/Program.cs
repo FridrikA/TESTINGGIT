6af7381c4efd4ed025f3deb662ed0154590f4ada
FridrikA <Frikadellerinn@gmail.com>
2016-02-12 01:44:08 +0000

    yow

--- Launasedill-1/Launasedill-1/Program.cs
+++ Launasedill-1/Launasedill-1/Program.cs
@@ -18,7 +18,7 @@ namespace Launasedill_1
 
 
         static void Main(string[] args)
-        {
+        {   
             decimal TaxPer1 = 0.3713m, TaxPer2 = 0.3835m, TaxPer3 = 0.4625m;
             decimal TaxAmount1 = 0m, TaxAmount2 = 0m, TaxAmount3 = 0m, TaxTotal = 0m;
             decimal TaxStep1 = 336035m, TaxStep2n3 = 836990m;
@@ -28,13 +28,38 @@ namespace Launasedill_1
             decimal Personal = 51920m;
             decimal SalaryAfterPens = 0m;
 
-         
-            Console.Write("Stimplaðu inn laun, engar kommur plís  ");
+            
+            Console.Write("Stimplaðu inn laun, engar kommur plís: ");
             Salary = Convert.ToDecimal(Console.ReadLine());
 
             Pens = Salary * PensPer;
             SalaryAfterPens = Salary - Pens;
 
+
+
+            
+            
+            if (SalaryAfterPens < TaxStep1)
+                TaxAmount1 = SalaryAfterPens * TaxPer1;
+            else if ((SalaryAfterPens > TaxStep1) && (SalaryAfterPens < TaxStep2n3))
+            {
+                TaxAmount1 = TaxStep1 * TaxPer1;
+                TaxAmount2 = (SalaryAfterPens - TaxStep1) * TaxPer2;
+            }
+            else if (SalaryAfterPens > TaxStep2n3)
+            {
+                TaxAmount1 = TaxStep1 * TaxPer1;
+                TaxAmount2 = (TaxStep2n3 - TaxStep1) * TaxPer2;
+                TaxAmount3 = (SalaryAfterPens - TaxStep2n3) * TaxPer3;
+            }
+
+            
+            //  Báðar virka. Þarf að velja hvor.
+            //  Á bara eftir að henda inn viðbótarsparnaði ezpz
+            //  ---------YAY----------------------
+
+
+            /*
             if (SalaryAfterPens > TaxStep1)
             {
                 if (SalaryAfterPens > TaxStep2n3)
@@ -42,34 +67,38 @@ namespace Launasedill_1
                     TaxAmount1 = TaxStep1 * TaxPer1;
                     TaxAmount2 = (TaxStep2n3 - TaxStep1) * TaxPer2;
                     TaxAmount3 = (SalaryAfterPens - TaxStep2n3) * TaxPer3;
-                }
 
+                }
                 else
+                {
                     TaxAmount1 = TaxStep1 * TaxPer1;
                     TaxAmount2 = (SalaryAfterPens - TaxStep1) * TaxPer2;
+                }
             }
-
-            if (SalaryAfterPens < TaxStep1)
-                TaxAmount1 = SalaryAfterPens * TaxPer1;  
+            else
+                TaxAmount1 = SalaryAfterPens * TaxPer1;
+            */
 
             
+            
+
             Union = Salary * UnionPer;
             TaxTotal = TaxAmount1 + TaxAmount2 + TaxAmount3 - Personal;
             SalaryAfterTax = SalaryAfterPens - TaxTotal - Union;
 
 
 
-            /*
+            
             Console.WriteLine("SKATTÞREP 1 {0}", TaxAmount1);
             Console.WriteLine("SKATTÞREP 2 {0}", TaxAmount2);
             Console.WriteLine("SKATTÞREP 3 {0}", TaxAmount3);
-            */
+            
 
-            Console.WriteLine("Greitt í lífseyrissjóð: {0}", Pens);
-            Console.WriteLine("Greitt í stéttarfélag: {0}", Union);
-            Console.WriteLine("Skattstofn {0}", SalaryAfterPens);
-            Console.WriteLine("Skattur eftir frádrátt persónuafsláttar {0}", TaxTotal);
-            Console.WriteLine("Útborguð laun eru {0} ", SalaryAfterTax);
+            Console.WriteLine("Greitt í lífseyrissjóð: {0}", Math.Round(Pens));
+            Console.WriteLine("Greitt í stéttarfélag: {0}", Math.Round(Union));
+            Console.WriteLine("Skattstofn {0}", Math.Round(SalaryAfterPens));
+            Console.WriteLine("Skattur eftir frádrátt persónuafsláttar {0}", Math.Round(TaxTotal));
+            Console.WriteLine("Útborguð laun eru {0} ", Math.Round(SalaryAfterTax));
             
             Console.ReadLine();
 
@@ -86,6 +115,7 @@ namespace Launasedill_1
                 Yfirvinna
                 Viðbótarsparnaður
                 Persónuafsláttur
+
                  //Find the total tax paid
    
                  //Find the pension amount
